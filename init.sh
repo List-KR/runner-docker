21c38c785f157c9dd2212388f0cd40c9f38e9764
PiQuark6046 <piquark6046@proton.me>
2023-01-29 10:46:59 +0000

    Fix invalid syntax

--- init.sh
+++ init.sh
@@ -16,8 +16,8 @@ do
   npm i -g "$i"
 done
 
-dnslookupver=${{ curl https://api.github.com/repos/ameshkov/dnslookup/releases/latest -s | jq .name -r | grep -Po '[0-9]+\.[0-9]+\.[0-9]+$' }}
-wget https://github.com/ameshkov/dnslookup/releases/download/v${{dnslookupver}}/dnslookup-linux-amd64-v${{dnslookupver}}.tar.gz -o dnslookup.tar.gz
+dnslookupver="$(curl https://api.github.com/repos/ameshkov/dnslookup/releases/latest -s | jq .name -r | grep -Po '[0-9]+\.[0-9]+\.[0-9]+$')"
+wget https://github.com/ameshkov/dnslookup/releases/download/v"${{dnslookupver}}"/dnslookup-linux-amd64-v"${{dnslookupver}}".tar.gz -o dnslookup.tar.gz
 tar -xvzf dnslookup.tar.gz -C dnslookup
 mv dnslookup/linux-amd64/dnslookup /bin/dnslookup
 chmod +x /bin/dnslookup
