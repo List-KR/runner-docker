7fcb0e9cc43309dd60409210d68b2c3dbab52f96
PiQuark6046 <piquark6046@proton.me>
2023-02-11 07:03:45 +0000

    Add regexp-tree

--- init.sh
+++ init.sh
@@ -20,7 +20,7 @@ bash dotnet-install.sh --channel STS
 rm dotnet-install.sh
 
 # npm packages
-NpmPackages=("@actions/core" "@actions/github" "@actions/exec" "selenium-webdriver" "node-json-db" "ts-node" "tslib" "typescript" "adm-zip" "chance" "safe-regex" "benchmark" "openpgp" "sha.js")
+NpmPackages=("@actions/core" "@actions/github" "@actions/exec" "selenium-webdriver" "node-json-db" "ts-node" "tslib" "typescript" "adm-zip" "chance" "safe-regex" "benchmark" "openpgp" "sha.js" "regexp-tree")
 NpmDevPackges=("@types/safe-regex" "@types/node" "@types/chance" "@types/benchmark" "@types/selenium-webdriver" "@types/adm-zip" "@types/sha.js")
 
 for i in "${NpmPackages[@]}"
